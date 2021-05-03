7dcf6cacb06db7299df833397d576efd2b90047f
Kajetan Boroszko <kajetan.boroszko@gmail.com>
2021-05-03 22:17:34 +0200

    fixed excitation

--- excitation_gcn.py
+++ excitation_gcn.py
@@ -50,24 +50,24 @@ import matplotlib.pyplot as plt
 # %%
 
 # create artifical data feature (= in degree) for each node
-# def create_artificial_features(dataset):
-#     for (graph, _) in dataset:
-#         graph.ndata['feat'] = graph.in_degrees().view(-1, 1).float()
-#         graph.edata['feat'] = torch.ones(graph.number_of_edges(), 1)
-#     return dataset
+def create_artificial_features(dataset):
+    for (graph, _) in dataset:
+        graph.ndata['feat'] = graph.in_degrees().view(-1, 1).float()
+        graph.edata['feat'] = torch.ones(graph.number_of_edges(), 1)
+    return dataset
 
 
 
 # %%
 
 # Generate artifical graph dataset with DGL
-# trainset = MiniGCDataset(350, 10, 20)
-# testset = MiniGCDataset(100, 10, 20)
-#
-# trainset = create_artificial_features(trainset)
-# testset = create_artificial_features(testset)
-#
-# print(trainset[0])
+trainset = MiniGCDataset(350, 10, 20)
+testset = MiniGCDataset(100, 10, 20)
+
+trainset = create_artificial_features(trainset)
+testset = create_artificial_features(testset)
+
+print(trainset[0])
 
 # %%
 
@@ -109,21 +109,24 @@ class ExcitationGCN_layer(nn.Module):
 
     def forward(self, g, X, E_X, snorm_n, snorm_e):
 
-        avg_pool = torch.sum(X, dim=0)
+        g.ndata['H'] = X
+        avg_pool = dgl.mean_nodes(g, 'H')
         ex = self.FC1(avg_pool)
         ex = torch.relu(ex)
         ex = self.FC2(ex)
         ex = torch.sigmoid(ex)
 
-        g.ndata['H'] = X
+        g_batch = []
+        for i,ng in enumerate(dgl.unbatch(g)):
+            ng.ndata['BX'] = torch.mul(ex[i], ng.ndata['H'])
+            g_batch.append(ng)
+        g = dgl.batch(g_batch)
+
         g.ndata['AX'] = self.A(X)
-        g.ndata['BX'] = torch.mul(ex, self.B(X))
         g.ndata['DX'] = self.D(X)
         g.ndata['EX'] = self.E(X)
         g.edata['E'] = E_X
         g.edata['CE'] = self.C(E_X)
-
-        g.ndata['BX_alpha'] = self.B(X)
 
         g.update_all(self.message_func, self.reduce_func)
 
@@ -155,7 +158,6 @@ class ExcitationGCN(nn.Module):
             ExcitationGCN_layer(hidden_dim, hidden_dim) for _ in range(L)
         ])
         self.MLP_layer = gated_gcn.MLP_layer(hidden_dim, output_dim)
-        self.softmax = torch.nn.Softmax(dim=1)
 
     def forward(self, g, X, E, snorm_n, snorm_e):
         # input embedding
@@ -170,7 +172,6 @@ class ExcitationGCN(nn.Module):
         g.ndata['H'] = H
         y = dgl.mean_nodes(g, 'H')
         y = self.MLP_layer(y)
-        y = self.softmax(y)
 
         return y
 
@@ -180,8 +181,8 @@ class ExcitationGCN(nn.Module):
 # %%
 #
 # # instantiate network
-# model = ExcitationGCN(input_dim=1, hidden_dim=150, output_dim=8, L=2)
-# print(model)
+model = ExcitationGCN(input_dim=1, hidden_dim=150, output_dim=8, L=2)
+print(model)
 
 
 # %% md
@@ -192,27 +193,27 @@ class ExcitationGCN(nn.Module):
 
 # Collate function to prepare graphs
 #
-# def collate(samples):
-#     graphs, labels = map(list, zip(*samples))  # samples is a list of pairs (graph, label)
-#     labels = torch.tensor(labels)
-#     sizes_n = [graph.number_of_nodes() for graph in graphs]  # graph sizes
-#     snorm_n = [torch.FloatTensor(size, 1).fill_(1 / size) for size in sizes_n]
-#     snorm_n = torch.cat(snorm_n).sqrt()  # graph size normalization
-#     sizes_e = [graph.number_of_edges() for graph in graphs]  # nb of edges
-#     snorm_e = [torch.FloatTensor(size, 1).fill_(1 / size) for size in sizes_e]
-#     snorm_e = torch.cat(snorm_e).sqrt()  # graph size normalization
-#     batched_graph = dgl.batch(graphs)  # batch graphs
-#     return batched_graph, labels, snorm_n, snorm_e
+def collate(samples):
+    graphs, labels = map(list, zip(*samples))  # samples is a list of pairs (graph, label)
+    labels = torch.tensor(labels)
+    sizes_n = [graph.number_of_nodes() for graph in graphs]  # graph sizes
+    snorm_n = [torch.FloatTensor(size, 1).fill_(1 / size) for size in sizes_n]
+    snorm_n = torch.cat(snorm_n).sqrt()  # graph size normalization
+    sizes_e = [graph.number_of_edges() for graph in graphs]  # nb of edges
+    snorm_e = [torch.FloatTensor(size, 1).fill_(1 / size) for size in sizes_e]
+    snorm_e = torch.cat(snorm_e).sqrt()  # graph size normalization
+    batched_graph = dgl.batch(graphs)  # batch graphs
+    return batched_graph, labels, snorm_n, snorm_e
 
 
 # %%
 
 # Compute accuracy
 #
-# def accuracy(logits, targets):
-#     preds = logits.detach().argmax(dim=1)
-#     acc = (preds == targets).sum().item()
-#     return acc
+def accuracy(logits, targets):
+    preds = logits.detach().argmax(dim=1)
+    acc = (preds == targets).sum().item()
+    return acc
 
 
 # %% md
@@ -223,28 +224,28 @@ class ExcitationGCN(nn.Module):
 
 # Define DataLoader and get first graph batch
 #
-# train_loader = DataLoader(trainset, batch_size=10, shuffle=True, collate_fn=collate)
-# batch_graphs, batch_labels, batch_snorm_n, batch_snorm_e = next(iter(train_loader))
-# batch_X = batch_graphs.ndata['feat']
-# batch_E = batch_graphs.edata['feat']
+train_loader = DataLoader(trainset, batch_size=10, shuffle=True, collate_fn=collate)
+batch_graphs, batch_labels, batch_snorm_n, batch_snorm_e = next(iter(train_loader))
+batch_X = batch_graphs.ndata['feat']
+batch_E = batch_graphs.edata['feat']
 
 
 # %%
 
 # Checking some sizes
 #
-# print(f'batch_graphs:', batch_graphs)
-# print(f'batch_labels:', batch_labels)
-# print('batch_X size:', batch_X.size())
-# print('batch_E size:', batch_E.size())
+print(f'batch_graphs:', batch_graphs)
+print(f'batch_labels:', batch_labels)
+print('batch_X size:', batch_X.size())
+print('batch_E size:', batch_E.size())
 
 # %%
 #
-# batch_scores = model(batch_graphs, batch_X, batch_E, batch_snorm_n, batch_snorm_e)
-# print(batch_scores.size())
+batch_scores = model(batch_graphs, batch_X, batch_E, batch_snorm_n, batch_snorm_e)
+print(batch_scores.size())
 
-# batch_labels = batch_labels
-# print(f'accuracy: {accuracy(batch_scores, batch_labels)}')
+batch_labels = batch_labels
+print(f'accuracy: {accuracy(batch_scores, batch_labels)}')
 
 # %% md
 
